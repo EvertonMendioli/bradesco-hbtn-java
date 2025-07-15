734493467c05ef8075db1767cc0de00d81e2711d
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-15 13:49:51 -0300

    task 4

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,71 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    private String nomeOriginal;
+    
+    public PersonagemGame() {
+    }
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        setSaudeAtual(saudeAtual);
+        this.nomeOriginal = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+
+        if(saudeAtual>0){
+            setStatus("vivo");
+        }else if(saudeAtual==0){
+            setStatus("morto");
+        }
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if(nome == null || nome.trim() == "") {
+            this.nome = this.nomeOriginal;
+        } else{
+
+        this.nome = nome;
+        }
+    }
+
+    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
+
+    public void tomarDano(int quantidadeDeDano){
+        int result = getSaudeAtual()-quantidadeDeDano;
+        if(result < 0 ){
+            result = 0;
+        }
+        
+        setSaudeAtual(result);
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        int result = getSaudeAtual() + quantidadeDeCura;
+        if(result > 100){
+            result = 100;
+        }
+        setSaudeAtual(result);
+    }
+
+}
