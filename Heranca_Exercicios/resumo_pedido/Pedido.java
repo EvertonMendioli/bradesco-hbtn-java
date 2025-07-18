3b84acef1b9dc30c62a778c8c5e2d9b3d017df66
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-18 00:25:32 -0300

    exercicio 2

---
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -0,0 +1,87 @@
+import produtos.Dvd;
+import produtos.Produto;
+
+public class Pedido {
+
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+
+    public double calcularTotal() {
+        double qtdeDvd = 0;
+        double qtdeLivro = 0;
+        
+        
+        for (ItemPedido itemPedido : itens) {
+            if(itemPedido.getProduto().getClass().getName().equals("produtos.Dvd")){   
+                 qtdeDvd += itemPedido.getQuantidade() * itemPedido.getProduto().obterPrecoLiquido();
+                
+            }
+
+            if(itemPedido.getProduto().getClass().getName().equals("produtos.Livro")){
+                qtdeLivro += itemPedido.getQuantidade()* itemPedido.getProduto().obterPrecoLiquido();;
+            }
+        }
+
+        return (qtdeDvd + qtdeLivro) - ((qtdeDvd + qtdeLivro) * (getPercentualDesconto()/100));
+    }
+
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void apresentarResumoPedido(){
+       String tipo = "";
+       String teste = "";
+       int qtde = 0;
+       double total = 0;
+        System.out.printf("------- RESUMO PEDIDO -------");
+for (ItemPedido itemPedido : itens) {
+            
+          if(itemPedido.getProduto().getClass().getName().equals("produtos.Dvd")){   
+                 tipo = "Dvd"; 
+            }
+
+            if(itemPedido.getProduto().getClass().getName().equals("produtos.Livro")){
+                tipo = "Livro";
+            }
+            qtde += itemPedido.getQuantidade();
+            total += itemPedido.getProduto().getPrecoBruto() * itemPedido.getQuantidade();
+            //qtdeLivro += itemPedido.getQuantidade()* itemPedido.getProduto().obterPrecoLiquido();
+          System.out.printf("\nTipo: " + tipo + 
+        "  Titulo: " + itemPedido.getProduto().getTitulo() + 
+        "  Preco: "   + itemPedido.getProduto().getPrecoBruto() + 
+        "  Quantidade: " + itemPedido.getQuantidade() + 
+        "  Total:  " + itemPedido.getProduto().getPrecoBruto() * itemPedido.getQuantidade());
+        
+
+
+        }
+        double totalGeral= total - (total * (getPercentualDesconto()/100));
+        String resultado = 
+        "\n----------------------------\n" 
+        +"DESCONTO: " + total * (getPercentualDesconto()/100)
+        +"\nTOTAL PRODUTOS:  " + total 
+        + "----------------------------\n" 
+        +"TOTAL PEDIDO: "   + totalGeral;
+
+        System.out.printf(resultado);
+
+       
+       
+       
+        
+        
+
+
+    }
+
+
+    
+}
---
+++ Heranca_Exercicios/resumo_pedido/produtos/Produto.java
@@ -0,0 +1,28 @@
+package produtos;
+public abstract class Produto {
+    public abstract double obterPrecoLiquido();
+
+    private String titulo;
+    private int ano;
+    private String pais;
+    private double precoBruto;
+
+    public Produto(String titulo, int ano, String pais, double precoBruto) {
+        this.titulo = titulo;
+        this.ano = ano;
+        this.pais = pais;
+        this.precoBruto = precoBruto;
+    }
+
+    public  double getPrecoBruto() {
+        return precoBruto;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    
+
+    
+
+}
