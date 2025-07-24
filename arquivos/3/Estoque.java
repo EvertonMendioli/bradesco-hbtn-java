088bd3c101cdc52f95b7cf9bb18fe7befd14bf01
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-24 00:14:55 -0300

    arquivo 3

---
+++ arquivos/3/Estoque.java
@@ -0,0 +1,88 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class Estoque {
+
+    private String nomeArquivo;
+
+    public Estoque(String nomeArquivo) {
+        this.nomeArquivo = nomeArquivo;
+    }
+
+    public void adicionarProduto(String nome, int quantidade, double preco) {
+
+        try {
+            FileWriter fileWriter = new FileWriter(this.nomeArquivo);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(1 + "," + nome + "," + quantidade + "," + preco);
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    public void excluirProduto(int idExcluir) {
+
+    }
+
+    public void exibirEstoque() {
+        try {
+            FileReader fileReader = new FileReader(nomeArquivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    public void atualizarQuantidade(int idAtualizar, int novaQuantidade) {
+        String nome;
+        double preco;
+        try {
+            FileReader fileReader = new FileReader(nomeArquivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileWriter fileWriter = new FileWriter("temp.csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] valor = linha.split(",");
+
+                if (Integer.parseInt(valor[0].trim()) == idAtualizar) {
+                    nome = valor[1];
+                    preco = Double.parseDouble(valor[3]);
+                    bufferedWriter.write(idAtualizar + "," + nome + ", " + novaQuantidade + "," + preco);
+                    bufferedWriter.newLine();
+                } else {
+                    bufferedWriter.write(linha);
+                    bufferedWriter.newLine();
+                }
+
+            }
+            bufferedWriter.close();
+
+            Path nomeArquivoOriginal = Paths.get(nomeArquivo);
+            File novoArquivo = new File("temp.csv");
+            
+            if (Files.delete(nomeArquivoOriginal)) {
+                novoArquivo.renameTo(new File(nomeArquivo));
+            }else{
+                System.out.println("não deu certo");
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+    }
+}
