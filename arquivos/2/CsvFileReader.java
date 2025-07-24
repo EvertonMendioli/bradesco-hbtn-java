2b2594c0f76e4f9735e90fe47cd4526b763c8629
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-23 22:10:53 -0300

    arquivos 2

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -5,31 +5,26 @@ import java.io.IOException;
 public class CsvFileReader {
 
     public static void main(String[] args) {
-    try {
-        FileReader fileReader = new FileReader("funcionarios.csv");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        String linha;
-        while ((linha = bufferedReader.readLine())!= null) {
-        String[] valor = linha.split(",");
+        try {
+            FileReader fileReader = new FileReader("funcionarios.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linha;
+            while ((linha = bufferedReader.readLine()) != null) {
+                String[] valor = linha.split(",");
 
-           System.out.println("Funcionario: " + valor[0].trim());
-           System.out.println("Idade: " + valor[1].trim());
-           System.out.println("Departamento: " + valor[2].trim());
-           System.out.println("Salarial: " + valor[3].trim());
-           System.out.println("------------------------");
+                System.out.println("Funcionario: " + valor[0].trim());
+                System.out.println("Idade: " + valor[1].trim());
+                System.out.println("Departamento: " + valor[2].trim());
+                System.out.println("Salarial: " + valor[3].trim());
+                System.out.println("------------------------");
 
-        }
-        System.out.println("Leitura do arquivo concluída.");
-        System.out.println("");
-        
-        
-        
-
-
-    } catch (IOException e) {
-                
-                e.printStackTrace();
             }
-     
-}
+            System.out.println("Leitura do arquivo concluída.");
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
