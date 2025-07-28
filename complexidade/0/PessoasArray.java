6af30e282639f13eba45906715b8fa690a812302
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-28 09:13:28 -0300

    complexidade 0

---
+++ complexidade/0/PessoasArray.java
@@ -0,0 +1,33 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de pesquisaConstante
+    public void pesquisaTempoConstante(int id){
+        
+        if(id<=22){
+        String [] teste = getNomes();
+        String teste2 = teste[id];  
+        System.out.println("Nome pesquisado é " + teste2 + " que está na posição "+id);
+        }else{
+            throw new IllegalArgumentException("O array de nomes possui :22 nomes.");
+
+        }
+    }
+    }
+
