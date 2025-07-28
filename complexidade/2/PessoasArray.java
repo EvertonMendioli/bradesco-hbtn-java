62712fbb766070183d746e641194d31c77830d96
EvertonMendioli <evertonbaixista@gmail.com>
2025-07-28 20:19:51 -0300

    complexidade 2

--- complexidade/2/PessoasArray.java
+++ complexidade/2/PessoasArray.java
@@ -20,6 +20,29 @@ public class PessoasArray {
     // implementar o método de buscaBinaria
     public void buscaBinaria(String nome){
         boolean encontrado = false;
+        int inicio= 0;
+        int fim = nomes.length - 1;
+        int valorMeio;
+        System.out.println("Procurando o nome: \"" + nome + "\"");
+        for(int i = 0; i<nomes.length; i++ ){
+         valorMeio = (inicio + fim)/2;
+            System.out.println("Passando pelo indice:" + valorMeio);
+
+            if (valorMeio==0) {
+                throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+            }
+
+            if(nomes[valorMeio] == nome){
+                encontrado = true;
+                System.out.println("Nome "+ nome  + " encontrado na posição " + valorMeio);
+                break;
+            }else if(nomes[valorMeio].compareTo(nome) < 0){
+                inicio = valorMeio + 1;
+            }else{
+                fim = valorMeio - 1;
+            }
+        }
+
 
     }
 }
