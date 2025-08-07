5cccea358ffacd283b24e8ec44362612b3bf4c8c
EvertonMendioli <evertonbaixista@gmail.com>
2025-08-07 12:14:36 +0000

    Collectors 0

---
+++ 0x19/pessoa_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas){
+
+        Map<String, List<Pessoa>> agrupados = pessoas.stream().collect(Collectors.groupingBy(Pessoa :: getCargo));
+            
+        
+
+
+        return agrupados;
+    }
+}
