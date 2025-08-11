48f42c42ac3a5f8bd9cca37f78de61c6421854b5
EvertonMendioli <evertonbaixista@gmail.com>
2025-08-11 09:30:19 -0300

    Streams, Collectors e Optional 2

---
+++ Streams_Collector_Optional_Exercicios/hobbies_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,64 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+
+        TreeMap<String, TreeSet<Pessoa>> ordemReversa = pessoas.stream()
+                .collect(Collectors.groupingBy(
+                        Pessoa::getCargo,
+                        () -> new TreeMap<>(Comparator.reverseOrder()), Collectors.toCollection(TreeSet::new)));
+
+        return ordemReversa;
+
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas){
+
+        Map<String, Long> contagem = pessoas.stream()
+        .collect(Collectors.groupingBy(
+            Pessoa::getCargo, Collectors.counting()));    
+        
+        return contagem;
+
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas){
+
+        Map<String, Map<Integer, Long>> contagem = pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, 
+                 Collectors.groupingBy(Pessoa :: getIdade, Collectors.counting())));
+
+        return contagem;
+
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas){
+
+        Map<String, Double> contagem = pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, 
+                 Collectors.averagingDouble(Pessoa :: getSalario))); 
+
+
+        return contagem;
+    }    
+
+    public static Map<String, TreeSet<String>> obterHobbiesPorCargo(List<Pessoa> pessoas){
+    
+        Map<String, TreeSet<String>> contagem = pessoas.stream()
+        .collect(Collectors.groupingBy(Pessoa::getCargo, 
+                Collectors.toCollection(Pessoa :: getHobbies),   Collectors.toCollection(TreeSet::new)
+                )); 
+
+
+        return contagem;
+    
+    }
+
+
+}
