038e8c071158ea1686607fa927f95e7de229ab93
EvertonMendioli <evertonbaixista@gmail.com>
2025-08-19 11:45:03 +0000

    threads 0

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -2,15 +2,19 @@
 public class Principal{
     
     
-   public static void main(String[] args){
+   public static void main(String[] args) throws InterruptedException{
 
         Contador contador = new Contador();
 
         ThreadContador threads = new ThreadContador(contador);
         threads.start();
-        threads.start();
-        //threads.join();
 
+        ThreadContador threads1 = new ThreadContador(contador);
+        threads1.start();
+
+            threads.join();
+            threads1.join();
+ 
         System.out.println("Valor final do contador: " + contador.getContagem());
 
     }
